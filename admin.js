cad272b82beed7f4a5a38ac7a6d13561731df5e7
Alpamys Kanibetov <alpamys.kanibetov@bee.kz>
2013-04-03 11:56:49 +0600

    admin works finally without validation

--- admin.js
+++ admin.js
@@ -19,8 +19,44 @@ http.createServer(function(request, response) {
       response.write(JSON.stringify(config));
       response.end();   
     }
-    esle if (lVars.query.action == 'save') {
+    else if (lVars.query.action == 'save') {
+      console.log('host ' + lVars.query.host);
+      console.log('ip ' + lVars.query.ip);
+      console.log('port ' + lVars.query.port);
+      console.log('type ' + lVars.query.type);
 
+      var file = fs.readFileSync(__dirname + '/lib/tracker/settings.json', "utf8");
+      var config = JSON.parse(file);
+
+      var data = lVars.query.host.split(':');
+
+      for (var i in config) {
+        var server = config[i];
+
+        if (server.ip == data[0]  && server.port == data[1]) {
+           server.ip = lVars.query.ip;
+           server.port = lVars.query.port;
+           server.type = lVars.query.type;
+           break;
+        } 
+      }
+
+      console.log( JSON.stringify(config) );
+
+      fs.writeFile(__dirname + '/lib/tracker/settings.json', JSON.stringify(config) , function(err) {
+          if(err) {
+              console.log(err);
+          } else {
+              console.log("The file was saved!");
+          }
+      });
+
+      response.writeHead(200,{
+                  'Content-Type': 'application/json',
+                  'Access-Control-Allow-Origin': '*'
+              });
+      response.write(JSON.stringify(config));
+      response.end();
     }
   }
 
